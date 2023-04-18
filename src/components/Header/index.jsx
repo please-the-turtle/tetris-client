4b6820bb9f5d31532814dded843b67ce203c1610
Alexey Viklov <alexey.viklov@gmail.com>
2023-04-18 21:46:08 +0300

    Header component changed.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,66 @@
+import React, { useState } from 'react';
+import useWindowSize from '../../hooks/useWindowSize';
 import './Header.scss';
+import '../../_vars.scss';
 import { Link } from 'react-router-dom';
+import * as IoIcons from 'react-icons/io';
+import { IconContext } from 'react-icons';
 
 export default function Header() {
-    return (
-        <header className='header'>
-            <nav className='header__navigation'>
-                <Link to='' className='header__navigation__item header__navigation__item_bold'>🏡 Home</Link>
-                <Link to='friends' className='header__navigation__item'>friends</Link>
-                <Link to='statistics' className='header__navigation__item'>statistics</Link>
+    const windowSize = useWindowSize();
+    const [sidebar, setSidebar] = useState(false);
 
-                <Link to='game' className='header__navigation__item'>game page</Link>
-            </nav>
+    const showSidebar = () => setSidebar(!sidebar);
+
+    const navLinksData = [
+        {
+            title: "Home",
+            path: "/",
+            cName: "header__navigation__item header__navigation__item_bold"
+        },
+        {
+            title: "friends",
+            path: "friends",
+            cName: "header__navigation__item"
+        },
+        {
+            title: "statistics",
+            path: "statistics",
+            cName: "header__navigation__item"
+        },
+        {
+            title: "game",
+            path: "game",
+            cName: "header__navigation__item"
+        },
+    ];
+
+    const showNavLinks = () => navLinksData.map((item, index) => {
+        return (
+            <Link key={index} to={item.path} className={item.cName}>
+                <span>{item.title}</span>
+            </Link>
+        )
+    });
+
+    return (
+        <header>
+            <IconContext.Provider value={{ color: '#fff' }}>
+                {(windowSize.width > 480) ? (
+                    <nav className='header__navigation'>
+                        {showNavLinks()}
+                    </nav>) : (
+                    <div className=''>
+                        <button className='sidebar__button' onClick={showSidebar}>
+                            {sidebar ? <IoIcons.IoMdClose /> : <IoIcons.IoMdMenu />}
+                        </button>
+                        <nav className={sidebar ? 'sidebar active' : 'sidebar'}
+                            onClick={showSidebar}>
+                            {showNavLinks()}
+                        </nav>
+                    </div>
+                )}
+            </IconContext.Provider>
         </header>
     )
 }
