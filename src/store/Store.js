1cd39b85211e7def862693bbfc8ba671beecf855
Alexey Viklov <alexey.viklov@gmail.com>
2023-05-23 12:43:47 +0300

    Login and register functions in Store.js changed.

--- src/store/Store.js
+++ src/store/Store.js
@@ -14,15 +14,25 @@ export default function Store() {
     });
 
     async function login(username, password) {
-        const response = await authService.login(username, password);
-        localStorage.setItem('token', response.data.accessToken);
-        setAutorized(true);
-        return response;
+        try {
+            const response = await authService.login(username, password);
+            localStorage.setItem('token', response.data.accessToken);
+            setAutorized(true);
+            return response;
+        } catch (e) {
+            console.log(e.response?.data?.message);
+            if (e.response) throw e;
+        }
     };
 
     async function register(username, password) {
-        const response = await authService.register(username, password);
-        return response;
+        try {
+            const response = await authService.register(username, password);
+            return response;
+        } catch (e) {
+            console.log(e.response?.data?.message);
+            if (e.response) throw e;
+        }
     };
 
     async function logout() {
